6043720a221ad4c5c4ee4daa2cad0c6c7afcdae8
pngwn <hello@pngwn.io>
2020-05-25 17:22:54 +0100

    styling tweaks, close button for menu

--- src/routes/docs.svelte
+++ src/routes/docs.svelte
@@ -39,6 +39,7 @@
       ],
     ],
     ["Frontmatter", "docs#frontmatter-1"],
+    // ["Integrations", "docs#integrations"],
     ["Limitations", "docs#limitations"],
   ];
 
@@ -336,17 +337,27 @@
       right: 1.8rem;
       z-index: 999;
       height: 3.2rem;
-      padding: 5px 7px;
+      padding: 6px 7px 6px 7px;
       background: #fff;
       box-sizing: border-box;
       border-radius: 3px;
       /* border: 1px solid grey; */
       box-shadow: 0 1px 5px rgba(0, 0, 0, 0.15);
+      cursor: pointer;
     }
 
     .icon {
       width: 2rem;
+      height: 2rem;
       display: inline-block;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .icon svg {
+      width: 100%;
+      height: 100%;
     }
 
     li.solo:first-child {
@@ -399,25 +410,47 @@
 {#if width < 1100}
   <span class="menu" on:click={() => (menu_show = !menu_show)}>
     <span class="icon">
-      <svg
-        aria-hidden="true"
-        focusable="false"
-        data-prefix="fas"
-        data-icon="bars"
-        class="svg-inline--fa fa-bars fa-w-14"
-        role="img"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 448 512">
-        <path
-          fill="currentColor"
-          d="M16 132h416c8.837 0 16-7.163
-          16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837
-          7.163 16 16 16zm0 160h416c8.837 0 16-7.163
-          16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837
-          7.163 16 16 16zm0 160h416c8.837 0 16-7.163
-          16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837
-          7.163 16 16 16z" />
-      </svg>
+      {#if !menu_show}
+        <svg
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fas"
+          data-icon="bars"
+          class="svg-inline--fa fa-bars fa-w-14"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512">
+          <path
+            fill="currentColor"
+            d="M16 132h416c8.837 0 16-7.163
+            16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837
+            7.163 16 16 16zm0 160h416c8.837 0 16-7.163
+            16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0
+            8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163
+            16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0
+            8.837 7.163 16 16 16z" />
+        </svg>
+      {:else}
+        <svg
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fas"
+          data-icon="times"
+          class="svg-inline--fa fa-times fa-w-11"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 352 512">
+          <path
+            fill="currentColor"
+            d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19
+            0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28
+            75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28
+            12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28
+            32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176
+            322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48
+            0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" />
+        </svg>
+      {/if}
     </span>
   </span>
 {/if}
